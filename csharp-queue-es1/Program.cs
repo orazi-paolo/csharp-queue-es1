ba4c917fcdc5c1378336c573e41c05e4dd009b2e
orazi-paolo <orazi.paolo10@gmail.com>
2024-12-29 19:54:07 +0100

    first commit

---
+++ csharp-queue-es1/Program.cs
@@ -0,0 +1,12 @@
+﻿// dichiaro una coda di interi
+var queue = new Queue<int>(new[] { 1, 2, 3, 4 });
+
+// elimino il primo elemento
+queue.Dequeue();
+
+// stampo la coda aggiornata
+Console.WriteLine("Queue aggiornata:");
+foreach (int item in queue)
+{
+    Console.WriteLine(item);
+}
